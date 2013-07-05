5833d242a80b16b2935566eb71701c3561c65f6a
raydian <xp.java@gmail.com>
2013-07-06 00:23:43 +0800

    canvas

--- routes/canvas.js
+++ routes/canvas.js
@@ -2,10 +2,25 @@
 
 module.exports = function (app) {
     app.get('/', proc.index);
+    
+    app.get('/pt',proc.pngtest);
 };
 
 var proc = {};
 
 proc.index = function(req,res){
-	res.render('index',{title:'abcd'});
+	res.render('index',{title:'abcd',imgs:'abcdef'});
+};
+
+
+proc.pngtest = function(req,res){
+	var fs = require("fs"),
+    util = require("util");
+	var mime = require("mime");
+	var src = '../images/ggs.png';
+	var data = fs.readFileSync(src).toString("base64");
+	var dataUri = util.format("data:%s;base64,%s", mime.lookup(src), data);
+//	var dataUri = util.format("<img src=\"data:%s;base64,%s\">", mime.lookup(src), data);
+	
+	res.render('index',{title:'abcd',imgs:dataUri});
 };
